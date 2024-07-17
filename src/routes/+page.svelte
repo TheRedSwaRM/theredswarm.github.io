0531a600ccc05220fc965f72aaa241bb8ef3405f
TheRedSwaRM <rsbalatibat@up.edu.ph>
2024-07-18 01:21:41 +0800

    feat: install tailwind css and postcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="py-4">
+    <h1 class="text-4xl font-bold underline mt-4">
+        Hello, world! I'm Ram
+    </h1>
+</div>
+
+<!-- <style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style> -->
