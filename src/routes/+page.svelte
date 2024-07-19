d627bb415b8fccfb105b1b4b3c5b65856a849678
TheRedSwaRM <rsbalatibat@up.edu.ph>
2024-07-19 18:30:05 +0800

    chore: add temporary hardcoded outline markers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,29 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<div class="py-4">
-    <h1 class="text-4xl font-bold underline mt-4">
-        Hello, world! I'm Ram
+<div class="py-4" id="header">
+  <!-- insert navbar here -->
+</div>
+
+<div class="py-4" id="introduction">
+    <h1 class="text-4xl font-bold underline mt-4" >
+        Hello, world! I'm Ralf Michael <!-- add text above saying "but you can call me Ram!"-->
     </h1>
+    <p>A passionate and outgoing computer scientist committed to a life of learning and growth as I dive into various projects, ideas, or activities that revolve around Web Development, Software Development and Engineering, and Game Development.</p>
+</div>
+
+<div class="py-4" id="about_me">
+</div>
+
+<div class="py-4" id="projects">
+  <!-- insert here the projects I've made as well as a 3D interactable car that lets me travel through the various projects I've made starting from the most recent -->
+</div>
+
+<div class="py-4" id="contact_me">
+  <!-- insert contact info, FB page, linkedin, etc. -->
+</div>
+
+<div class="py-4" id="footer">
 </div>
 
 <!-- <style lang="postcss">
