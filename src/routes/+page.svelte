beac41eb2d464183fbd39bd77d51f5b9efe3a9fb
TheRedSwaRM <rsbalatibat@up.edu.ph>
2024-07-19 18:39:18 +0800

    chore: add comments as development reminders/notes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,23 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<!-- NOTE -->
+<!-- 
+- Responsive Design: site looks good on all devices.
+- Consistent Branding: consistent color scheme and typography.
+- Interactive Elements: animations or interactive elements. 
+-->
+
 <div class="py-4" id="header">
   <!-- insert navbar here -->
+  <!--  Header
+  Name and Title: Clearly state your name and your role (e.g., “John Doe - Software Developer” or “Jane Smith - Game Developer”).
+  Navigation Menu: Links to other sections of your site like About, Projects, Blog, and Contact. -->
 </div>
+
+<!-- Hero Section
+Introduction: A brief introduction or tagline that summarizes who you are and what you do.
+Call to Action: A button that encourages visitors to view your work or contact you. -->
 
 <div class="py-4" id="introduction">
     <h1 class="text-4xl font-bold underline mt-4" >
@@ -13,17 +27,29 @@
 </div>
 
 <div class="py-4" id="about_me">
+  <!-- About Me
+  Photo: A professional photo of yourself.
+  Bio: A short bio that highlights your background, skills, and interests.
+  Skills: A list of your key skills and technologies you are proficient in. -->
 </div>
 
 <div class="py-4" id="projects">
   <!-- insert here the projects I've made as well as a 3D interactable car that lets me travel through the various projects I've made starting from the most recent -->
+  <!-- Featured Projects
+  Project Thumbnails: Images or screenshots of your top projects.
+  Project Descriptions: Brief descriptions of each project, including your role, technologies used, and any notable achievements. -->
 </div>
 
-<div class="py-4" id="contact_me">
+<div class="py-4" id="contact_info">
   <!-- insert contact info, FB page, linkedin, etc. -->
+  <!-- Contact Form: A simple form for visitors to get in touch with you.
+  Social Media Links: Icons linking to your professional social media profiles (e.g., LinkedIn, GitHub, Twitter). -->
 </div>
 
 <div class="py-4" id="footer">
+  <!--  Footer
+  Quick Links: Additional navigation links.
+  Copyright Information: Basic copyright notice. -->
 </div>
 
 <!-- <style lang="postcss">
