7eed8e46caacbaf9d4fa27af8190de61271a5505
TheRedSwaRM <rsbalatibat@up.edu.ph>
2024-07-20 01:31:07 +0800

    feat: add Hero.svelte and Navbar.svelte component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+  import Navbar from "$lib/components/Navbar.svelte";
+  import Hero from "./Hero.svelte";
+
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
@@ -8,29 +13,25 @@
 - Interactive Elements: animations or interactive elements. 
 -->
 
+<div class="" id="navbar">
+  <Navbar />
+</div>
+
 <div class="py-4" id="header">
   <!-- insert navbar here -->
+
   <!--  Header
   Name and Title: Clearly state your name and your role (e.g., “John Doe - Software Developer” or “Jane Smith - Game Developer”).
   Navigation Menu: Links to other sections of your site like About, Projects, Blog, and Contact. -->
+
+  <Hero />
 </div>
 
 <!-- Hero Section
 Introduction: A brief introduction or tagline that summarizes who you are and what you do.
 Call to Action: A button that encourages visitors to view your work or contact you. -->
 
-<div class="py-4" id="introduction">
-    <h1 class="text-4xl font-bold underline mt-4" >
-        Hello, world! I'm Ralf Michael <!-- add text above saying "but you can call me Ram!"-->
-    </h1>
-    <p>A passionate and outgoing computer scientist committed to a life of learning and growth as I dive into various projects, ideas, or activities that revolve around Web Development, Software Development and Engineering, and Game Development.</p>
-</div>
-
-<div class="py-4" id="about_me">
-  <!-- About Me
-  Photo: A professional photo of yourself.
-  Bio: A short bio that highlights your background, skills, and interests.
-  Skills: A list of your key skills and technologies you are proficient in. -->
+<div class="py-4" id="languages_frameworks_technologies">
 </div>
 
 <div class="py-4" id="projects">
---
+++ src/routes/Hero.svelte
@@ -0,0 +1,16 @@
+
+<div class="py-4" id="Hero">
+    <div class="" id="left-side">
+        <h1 class="text-4xl font-bold underline mt-4" >
+            Hello, world! I'm Ralf Michael <!-- add text above saying "but you can call me Ram!"-->
+        </h1>
+        <h2>
+        Software Engineer, Game Developer, Passionate Learner, and Socially Enthusiastic <!-- add text above saying "but you can call me Ram!"-->
+        </h2>
+        <p>A passionate and outgoing computer scientist committed to a life of learning and growth as I dive into various projects, ideas, or activities that revolve around Web Development, Software Development and Engineering, and Game Development.</p>
+    </div>
+    <div class="" id="photo">
+        <!-- insert image here -->
+        <!-- insert background here -->
+    </div>
+</div>
