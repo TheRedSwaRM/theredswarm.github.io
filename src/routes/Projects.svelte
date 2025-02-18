68b39409b0897ff5ca8bca63ac4e12fa8ef63a39
TheRedSwaRM <rsbalatibat@up.edu.ph>
2025-02-18 20:38:25 +0800

    feat: add projects

--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -9,12 +9,30 @@
             <img class="rounded-t-lg w-full" src="{employability}" alt="" />
         </a>
         <div class="p-5">
-            <a href="/">
+            <a href="https://github.com/TheRedSwaRM/cs180-filipino-student-employability-predictor">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Filipino Student Employability Predictor</h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Project Description.</p>
-            <a href="/" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Project Link
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A short quiz that uses AI to assess your employability.</p>
+            <a href="https://github.com/TheRedSwaRM/cs180-filipino-student-employability-predictor" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                GitHub Repo
+                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+        </div>
+    </div>
+
+    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+        <a href="/">
+            <img class="rounded-t-lg w-full" src="{old}" alt="" />
+        </a>
+        <div class="p-5">
+            <a href="https://theredswarm.itch.io/project-privacy">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Project Privacy</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A short 2D point-and-click thriller game used in conducting research on the relationship between video game immersion and perceptions on Data Privacy.</p>
+            <a href="https://theredswarm.itch.io/project-privacy" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Game Page
                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                 </svg>
@@ -28,33 +46,15 @@
         </a>
         <div class="p-5">
             <a href="/">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Project Title</h5>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Super Ping Map</h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Project Description.</p>
-            <a href="/" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A content management system and campus network coverage visualizer developed during a workshop on software development hosted in the University Computer Center that</p>
+            <!-- <a href="/" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Project Link
                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                 </svg>
-            </a>
-        </div>
-    </div>
-
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-        <a href="/">
-            <img class="rounded-t-lg w-full" src="{old}" alt="" />
-        </a>
-        <div class="p-5">
-            <a href="/">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Project Title</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Project Description.</p>
-            <a href="/" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Project Link
-                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </a>
+            </a> -->
         </div>
     </div>
     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
@@ -62,50 +62,12 @@
             <img class="rounded-t-lg w-full" src="{old}" alt="" />
         </a>
         <div class="p-5">
-            <a href="/">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Project Title</h5>
+            <a href="https://github.com/TheRedSwaRM/smart-meter-iot">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">IoMeter: Smart Energy Meter</h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Project Description.</p>
-            <a href="/" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Project Link
-                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </a>
-        </div>
-    </div>
-    
-
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-        <a href="/">
-            <img class="rounded-t-lg" src="{old}" alt="" />
-        </a>
-        <div class="p-5">
-            <a href="/">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Project Title</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Project Description.</p>
-            <a href="/" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Project Link
-                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </a>
-        </div>
-    </div>
-    
-
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-        <a href="/">
-            <img class="rounded-t-lg" src="{old}" alt="" />
-        </a>
-        <div class="p-5">
-            <a href="/">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Project Title</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Project Description.</p>
-            <a href="/" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Project Link
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Real-time household energy cosumption tracking and alerting in the form of an IoT embedded system.</p>
+            <a href="https://github.com/TheRedSwaRM/smart-meter-iot" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Github Repo
                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                 </svg>
