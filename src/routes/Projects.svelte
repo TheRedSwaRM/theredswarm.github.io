043d0a27ee9cbe382cda4156ba20a9f8568224b3
TheRedSwaRM <rsbalatibat@up.edu.ph>
2025-02-23 18:04:31 +0800

    refactor: use ProjectCard modal for one of the projects instead of hardcode

--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -58,28 +58,18 @@
             </a> -->
         </div>
     </div>
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-        <a href="https://github.com/TheRedSwaRM/smart-meter-iot">
-            <img class="rounded-t-lg w-full" src="{old}" alt="" />
-        </a>
-        <div class="p-5">
-            <a href="https://github.com/TheRedSwaRM/smart-meter-iot">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">IoMeter: Smart Energy Meter</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Real-time household energy cosumption tracking and alerting in the form of an IoT embedded system.</p>
-            <a href="https://github.com/TheRedSwaRM/smart-meter-iot" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Github Repo
-                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </a>
-        </div>
-    </div>
-    <ProjectCard 
+    <ProjectCard
+        project_image="{old}"
+        project_title="IoMeter: Smart Energy Meter"
+        project_description="Real-time household energy cosumption tracking and alerting in the form of an IoT embedded system."
+        project_link="https://github.com/TheRedSwaRM/smart-meter-iot"
+        project_link_desc="Github Repository"
+    />
+    <ProjectCard
         project_image="{old}"
         project_title="Test"
         project_description="test project description"
-        project_link="test project link"
+        project_link="/"
         project_link_desc="test project linke description"
     />
 </div>
