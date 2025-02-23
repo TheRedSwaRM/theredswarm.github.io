2e2bb08728bae725f86f7dc218ee28350242e989
TheRedSwaRM <rsbalatibat@up.edu.ph>
2025-02-23 19:41:23 +0800

    refactor: replace hardcoded projects with more intuitive ProjectCard components

--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -4,60 +4,28 @@
     import employability from '$lib/assets/employability-predictor.png';
 </script>
 
-<div class="flex flex-wrap content-center justify-center gap-4 my-4">   
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-        <a href="https://github.com/TheRedSwaRM/cs180-filipino-student-employability-predictor">
-            <img class="rounded-t-lg w-full" src="{employability}" alt="" />
-        </a>
-        <div class="p-5">
-            <a href="https://github.com/TheRedSwaRM/cs180-filipino-student-employability-predictor">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Filipino Student Employability Predictor</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A short quiz that uses AI to assess your employability.</p>
-            <a href="https://github.com/TheRedSwaRM/cs180-filipino-student-employability-predictor" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                GitHub Repo
-                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </a>
-        </div>
-    </div>
-
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-        <a href="https://theredswarm.itch.io/project-privacy">
-            <img class="rounded-t-lg w-full" src="{old}" alt="" />
-        </a>
-        <div class="p-5">
-            <a href="https://theredswarm.itch.io/project-privacy">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Project Privacy</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A short 2D point-and-click thriller game used in conducting research on the relationship between video game immersion and perceptions on Data Privacy.</p>
-            <a href="https://theredswarm.itch.io/project-privacy" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Game Page
-                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </a>
-        </div>
-    </div>
-
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-        <a href="/">
-            <img class="rounded-t-lg w-full" src="{old}" alt="" />
-        </a>
-        <div class="p-5">
-            <a href="/">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Super Ping Map</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A content management system and campus network coverage visualizer developed during a workshop on software development hosted in the University Computer Center that</p>
-            <!-- <a href="/" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Project Link
-                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </a> -->
-        </div>
-    </div>
+<div class="flex flex-wrap content-center justify-center gap-4 my-4">
+    <ProjectCard
+        project_image="{employability}"
+        project_title="Filipino Student Employability Predictor"
+        project_description="A short quiz that uses AI to assess your employability."
+        project_link="https://github.com/TheRedSwaRM/cs180-filipino-student-employability-predictor"
+        project_link_desc="GitHub Repository"
+    />
+    <ProjectCard
+        project_image="{old}"
+        project_title="Project Privacy"
+        project_description="A short 2D point-and-click thriller game used in conducting research on the relationship between video game immersion and perceptions on Data Privacy."
+        project_link="https://theredswarm.itch.io/project-privacy"
+        project_link_desc="Game Page"
+    />
+    <ProjectCard
+        project_image="{old}"
+        project_title="UP Computer Center: Super Ping Map"
+        project_description="A content management system and campus network coverage visualizer developed during a workshop on software development hosted in the University Computer Center that"
+        project_link=""
+        project_link_desc=""
+    />
     <ProjectCard
         project_image="{old}"
         project_title="IoMeter: Smart Energy Meter"
@@ -67,9 +35,16 @@
     />
     <ProjectCard
         project_image="{old}"
-        project_title="Test"
-        project_description="test project description"
-        project_link="/"
-        project_link_desc="test project linke description"
+        project_title="ACCESS GRANTED"
+        project_description="A data science endeavor that explores the state of education access in the Philippines."
+        project_link="https://github.com/ssencarnacion/CS132-Project-Philippines-in-a-Nutshell---Education"
+        project_link_desc="Github Repository"
+    />
+    <ProjectCard
+        project_image="{old}"
+        project_title="Iwas Singko!"
+        project_description="An Arcade-like Game Set in UP Diliman"
+        project_link="https://github.com/TheRedSwaRM/IwasSingko"
+        project_link_desc="Github Repository"
     />
 </div>
