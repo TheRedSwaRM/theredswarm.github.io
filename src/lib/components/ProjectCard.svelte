d479d7aedc85992b27716df86cc96bc427d7b2aa
TheRedSwaRM <rsbalatibat@up.edu.ph>
2025-03-08 21:17:11 +0800

    chore: try to put project button to bottom of card

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -2,18 +2,20 @@
     export let project_image, project_title, project_description, project_link, project_link_desc: string;
 </script>
 
-<!-- TODO: Consistent formatting for card elements (button position, image sizes, etc.) -->
+<!-- TODO: move button position to the bottom -->
 
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-    <a href="{project_link}">
-        <img class="rounded-t-lg w-full h-1/2" src="{project_image}" alt="" />
+<div class="flex flex-col h-auto max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+    <a href="{project_link}" class="h-1/2">
+        <img class="rounded-t-lg w-full h-full" src="{project_image}" alt="" />
     </a>
-    <div class="p-5">
-        <a href="{project_link}">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{project_title}</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{project_description}</p>
-        <div class="align-end">
+    <div class="h-1/2">
+        <div class="p-5">
+            <a href="{project_link}">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{project_title}</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{project_description}</p>
+        </div>
+        <div class="px-5 pb-5">
             <a href="{project_link}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 {project_link_desc}
                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10">
